d8f2c9a496e8135b33702eef9ce57aec6d73ad8d
Khwaeesh-ashutec <121793446+Khwaeesh-ashutec@users.noreply.github.com>
2023-01-02 15:16:39 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackbliz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-kto2us)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Star iruyui orihg iorhel olj ter</h1>`;
